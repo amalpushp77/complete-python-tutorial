f6865e3b15d93add64d242e9e8f63f8766533e43
Amal <amalpushp97@gmail.com>
2021-06-16 20:59:37 +0530

    modified comments

--- 1. Python Part 1 - Basics/1. Python Character Sets/3. Operators.py
+++ 1. Python Part 1 - Basics/1. Python Character Sets/3. Operators.py
@@ -1,4 +1,13 @@
-#Operators in python visit this link - https://www.w3schools.com/python/python_operators.asp
+# Operators in python visit this link - https://www.w3schools.com/python/python_operators.asp
 
+""" Arithmetic operators
+    Assignment operators
+    Comparison operators
+    Logical operators
+    Identity operators
+    Membership operators
+    Bitwise operators"""
 
-#print('a' in 'program') #Membership operation, it will check for a in program if found it will give value True otherwise False
+# important
+# Membership operation, it will check for a in program if found it will give value True otherwise False
+# print('a' in 'program')
--- 1. Python Part 1 - Basics/1. Python Character Sets/4. First Program.py
+++ 1. Python Part 1 - Basics/1. Python Character Sets/4. First Program.py
@@ -1,5 +1,8 @@
-
 a = "Hello World"
-print(a)   #print the value of variable a
+print(a)   # print the value of variable a
 
 
+# a -> variable (identifier)
+# = -> assignment operator
+# "Hello world" -> string data type
+# print() -> built-in function
